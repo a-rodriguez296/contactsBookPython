6407dc342d3bcb35dc0fc7b0d5076988baa3f8f7
Alejandro <Ale@iMac-de-Liliana.local>
2019-11-29 11:05:19 -0500

    corriendo en flask

--- main.py
+++ main.py
@@ -11,5 +11,6 @@ def contact_book():
     return 'Hola mundo'
 
 
+#este comando sirve para correr el proyecto en Flask. Una vez incluyamos app-engine esto pierde validez
 if __name__ == '__main__':
     app.run()
