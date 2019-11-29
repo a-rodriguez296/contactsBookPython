28b193e642ddc5353ba98439101efc2703c69698
Alejandro <Ale@iMac-de-Liliana.local>
2019-11-29 11:30:39 -0500

    listo el formulario para crear un contacto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.debug = True
@@ -8,7 +8,15 @@ app.secret_key = 'some_secret'
 
 @app.route(r'/', methods=['GET'])
 def contact_book():
-    return render_template('index.html')
+    return render_template('contact_book.html')
+
+@app.route(r'/add', methods=['GET', 'POST'])
+def add_contact():
+    if request.form:
+        print(request.form.get('name'))
+        print(request.form.get('phone'))
+        print(request.form.get('email'))
+    return render_template('add_contact.html')
 
 
 #este comando sirve para correr el proyecto en Flask. Una vez incluyamos app-engine esto pierde validez
