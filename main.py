ec0732b5ec32f8bd6ecd4ec34543179c60f4c496
Alejandro <Ale@iMac-de-Liliana.local>
2019-11-29 11:16:12 -0500

    Ya hago un render de un template

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
@@ -8,7 +8,7 @@ app.secret_key = 'some_secret'
 
 @app.route(r'/', methods=['GET'])
 def contact_book():
-    return 'Hola mundo'
+    return render_template('index.html')
 
 
 #este comando sirve para correr el proyecto en Flask. Una vez incluyamos app-engine esto pierde validez
